db5a3fa4cc9816d0afc161f25e139e95232017bb
Hao Wu <hao.wu.fudan@gmail.com>
2018-08-13 17:37:18 -0400

    improved frame check efficiency

--- frame_check.py
+++ frame_check.py
@@ -1,25 +1,17 @@
 def find_bad_frame(data_set, check_progress=False, progress_bar=None):
     data_set_name = data_set.keys()[0][0]
     num_label = data_set[data_set_name].keys().levshape[0]
-    data_set_size = data_set.shape[0]
+
+    if check_progress:
+        progress_bar.setValue(50)
 
     labels = list()
     for label_id in range(num_label):
         labels.append(data_set[data_set_name].keys()[label_id*3][0])
 
-    bad_frames = list()
-    for i in range(data_set_size):
-        bad_frame = False
+    label_bad_frame = data_set[data_set_name][labels[0]]['likelihood'] < 0.95
+    for label in labels:
+        label_bad_frame = ((data_set[data_set_name][label]['likelihood'] < 0.95) | label_bad_frame)
 
-        for label in labels:
-            if data_set[data_set_name][label]['likelihood'][i] < 0.95:
-                bad_frame = True
-
-        if bad_frame:
-            bad_frames.append(i)
-
-        if check_progress:
-            if i%25 == 0:
-                progress_bar.setValue(i*100.0/data_set_size)
-
+    bad_frames = data_set.index[label_bad_frame].tolist()
     return bad_frames
